4d508fcaee82029b194a2d75d4d6d91a2a62d5bf
Ihor Pidhrabia <pidhrabia@gmail.com>
2022-08-02 22:08:58 +0300

    02.282022 22:09

--- login/login.php
+++ login/login.php
@@ -43,7 +43,6 @@
         </div>
     </form>
 
-    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="../slider/js/main.js"></script>
     <script src="../slider/js/slick.min.js"></script>
